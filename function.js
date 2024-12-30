3897bded26dc5b4fa5ae8943f8a319d47a459488
danish <danish@gmail.com>
2024-12-30 14:18:19 +0530

    functionss

--- function.js
+++ function.js
@@ -31,11 +31,93 @@
 
 
 
-function reversestring(str){
-    let reversedStr = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversedStr += str[i];
-    }
-    return reversedStr;
-}
-console.log(reversestring('hello'));
+// function reversestring(str){
+//     let reversedStr = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversedStr += str[i];
+//     }
+//     return reversedStr;
+// }
+// console.log(reversestring('hello'));
+
+
+// function palindromestring(str){
+//     let i = str.length -1;
+//     for( let i=0;  i < j / 2; i++) {
+//         let x = str[i];
+//         let y = str[j - i];
+//         if (x != y) {
+//             return false;
+//         }
+//         }
+//         return true;
+// }
+// console.log(palindromestring('racecar'));
+
+
+
+// function factorial(n)
+// {
+//     let result = 1;
+//     for (let i = 2; i <= n; i++)
+//         result = result * i;
+//     return result;
+// }
+// let num = 5;
+// console.log("Factorial of " + num + " is " + factorial(5))
+
+
+	
+	// function isVowel(ext) 
+    // { 
+	// 	ext = ext.toUpperCase(); 
+	// 	return ext == "A" || ext == "E" || ext == "I" || ext == "O" || ext == "U"; 
+	// } 
+
+	// function countVowels(str) 
+	// { 
+	// 	var count = 0; 
+	// 	for (var i = 0; i < str.length; i++) 
+	// 	if (isVowel(str[i])) 
+	// 		++count; 
+	// 	return count; 
+	// } 
+
+	// var str = "danish sharma"; 
+    // console.log(countVowels(str) );
+    
+		
+// function nthfibonacci(n){
+//     if(n<=1){
+//         return n;
+//     }
+//     return nthFibonacci(n + 1) + nthFibonacci(n + 2);
+// }
+// let n = 21;
+// console.log(nthfibonacci(n));
+
+
+
+// const sqrt5 = Math.sqrt(5);
+// const fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
+// console.log (Math.round(fibN / sqrt5))
+
+// function nthFibonacci(n) {
+//     if (n <= 1) {
+//         return n; 
+//     }
+//     return nthFibonacci(n - 1) + nthFibonacci(n - 2); 
+// }
+// let n = 21;
+// console.log(nthFibonacci(n)); 
+
+
+// function palindromestring(str){
+// 	let i=str length -1;
+// 	for( let i=0; i< J/2;i++ ){
+// 		let x = str[i]
+// 	}
+// }
+
+
+
