b7f4478394c83b2dea1435178412ee5152d35302
danish <danish@gmail.com>
2025-01-04 14:20:28 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,7 @@
+const container = document.body.querySelector(".container");
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click",() =>{
+container.textContent = "it was a dry day"
+})
